50681f72b122d9bc1e6fecdbdfd49082ce781860
David <dnaranjo89@gmail.com>
2020-08-30 17:40:36 +0200

    Rename project name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Cristina Crucianu Portfolio`,
+        short_name: `Cristina Crucianu Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
